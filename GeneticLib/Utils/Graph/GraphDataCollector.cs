357256b1854390e59b19e28730a4cb14132edec3
Emil Terman <terman.emil@gmail.com>
2018-05-23 15:58:32 +0300

    Added python fitness graph utils

---
+++ GeneticLib/Utils/Graph/GraphDataCollector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace GeneticLib.Utils.Graph
+{
+    public class GraphDataCollector
+    {
+		private List<Vector2> points = new List<Vector2>();
+        
+		public void Tick(int iteration, float value)
+		{
+			float lastVal;
+
+			if (points.Count() == 0)
+				lastVal = float.MaxValue;
+			else
+				lastVal = points.Last().Y;
+			
+			if (Math.Abs(lastVal - value) > float.Epsilon)
+				points.Add(new Vector2(iteration, value));
+		}
+
+		public void Draw(string graphTitle, string graphStyle = "b-")
+		{
+			PyDrawGraph.DrawGraph(points, graphTitle, graphStyle);
+		}
+    }
+}
---
+++ GeneticLib/Utils/Graph/PyDrawGraph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using Newtonsoft.Json;
+
+namespace GeneticLib.Utils.Graph
+{
+	public static class PyDrawGraph
+    {
+		static readonly string drawGraphPyFile =
+			"../GeneticLib/GeneticLib/Utils/Graph/DrawGraph.py";
+
+		public static void DrawGraph(
+			IEnumerable<Vector2> points,
+			string title,
+			string graph_style = "b-")
+		{
+			var jsonObj = new
+			{
+				graph_style,
+				title,
+				points = points.Select(p => new
+				{
+					x = p.X,
+					y = p.Y
+				})
+			};
+
+			var result = JsonConvert.SerializeObject(jsonObj);
+			result = result.Replace("\"", "\\\"")
+                           .Replace(" ", "")
+                           .Replace("\n", "");
+
+			RunPyGraph(result);
+		}
+
+		public static void RunPyGraph(string args)
+		{
+			using (var p = new Process())
+			{
+				var info = new ProcessStartInfo("python3");
+				info.Arguments = drawGraphPyFile + " " + args;
+				info.RedirectStandardInput = false;
+				info.RedirectStandardError = false;
+				info.RedirectStandardOutput = false;
+				info.UseShellExecute = false;
+
+				p.StartInfo = info;
+				p.Start();
+			}
+		}
+    }
+}
